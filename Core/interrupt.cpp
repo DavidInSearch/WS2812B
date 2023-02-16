b22b815722ba881895ef477c5f5aa0d507ed1f2c
DavidInSearch <davidogyan@gmail.com>
2023-02-16 14:01:18 +0400

    Start project

---
+++ Core/interrupt.cpp
@@ -0,0 +1,14 @@
+/*
+ * interrupt.cpp
+ *
+ *  Created on: Feb 16, 2023
+ *      Author: david
+ */
+
+#include "main.h"
+
+
+
+
+
+
---
+++ Core/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Feb 16, 2023
+ *      Author: david
+ */
+
+#include "main.h"
+
+uint32_t sysTickCntr = 0;
+
+int maincpp(){
+
+	while(true){
+
+	}
+}
+
+
+
+
